35d8d0436543ada8d82756d7005b17614ed6465b
Dunsin <ayodejiabisuwa23@gmail.com>
2025-07-14 07:32:23 +0100

    added cors allownace to next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Apply to all routes
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://yakihonne.com', // Whitelisted domain
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET, POST, OPTIONS',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
