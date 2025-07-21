8a5cdd60f1ef8a27bf5eb3b783134ca20688186b
Dunsin <ayodejiabisuwa23@gmail.com>
2025-07-21 09:51:40 +0100

    little styling

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from "next/navigation"
 import { IoIosArrowBack } from "react-icons/io";
 import SWhandler from "smart-widget-handler";
@@ -10,37 +10,37 @@ function Naviagtion() {
     const router = useRouter();
     const { userMetadata, setUserMetadata } = useClient();
     const [hostOrigin, setHostOrigin] = useState<string | null>(null);
-    
 
-       useEffect(() => {
-            SWhandler.client.ready();
-        }, []);
-    
-        // Listen for messages from host app
-        useEffect(() => {
-            let listener = SWhandler.client.listen((event) => {
-                if (event.kind === "user-metadata") {
-                    // Handle user metadata
-                    setUserMetadata(event.data?.user);
-                    console.log(event.data?.user);
-                    setHostOrigin(event.data?.host_origin);
-                }
-                if (event.kind === "err-msg") {
-                    // Handle error messages
-                    // setErrorMessage(event.data);
-                }
-                if (event.kind === "nostr-event") {
-                    // Handle Nostr events
-                    const { pubkey, id } = event.data?.event || {};
-                    // Process event data
-                }
-            });
-    
-            return () => {
-                // Clean up listener when component unmounts
-                listener?.close();
+
+    useEffect(() => {
+        SWhandler.client.ready();
+    }, []);
+
+    // Listen for messages from host app
+    useEffect(() => {
+        let listener = SWhandler.client.listen((event) => {
+            if (event.kind === "user-metadata") {
+                // Handle user metadata
+                setUserMetadata(event.data?.user);
+                console.log(event.data?.user);
+                setHostOrigin(event.data?.host_origin);
             }
-        }, []);
+            if (event.kind === "err-msg") {
+                // Handle error messages
+                // setErrorMessage(event.data);
+            }
+            if (event.kind === "nostr-event") {
+                // Handle Nostr events
+                const { pubkey, id } = event.data?.event || {};
+                // Process event data
+            }
+        });
+
+        return () => {
+            // Clean up listener when component unmounts
+            listener?.close();
+        }
+    }, []);
 
     return (
         <div className="flex flex-col gap-2">
@@ -58,9 +58,15 @@ function Naviagtion() {
                 <button onClick={() => router.push("create-content")} className="cursor-pointer bg-orange text-white rounded-sm border-1 p-3">create content</button>
             </div>
 
-            {/* show connectde user */}
-            <p>user - {userMetadata?.display_name}</p>
+            <div className="flex flex-col justify-center items-center gap-3">
 
+                <p>user - {userMetadata?.display_name}</p>
+                {/* show connectde user */}
+
+                <p>
+                    Earn Bitcoin by sharing pay-to-unlock content. Built on YakiHonne Smart Widgets, it shows a preview and zaps unlock the rest — simple, direct, and monetized while you sleep.
+                </p>
+            </div>
         </div>
     )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,9 @@
+const {
+    default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
@@ -26,5 +32,17 @@ module.exports = {
             },
         },
     },
-    plugins: [],
+    plugins: [require("tw-animate-css"), addVariablesForColors],
 };
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+    let allColors = flattenColorPalette(theme("colors"));
+    let newVars = Object.fromEntries(
+        Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+    );
+
+    addBase({
+        ":root": newVars,
+    });
+}
